1628dfcc216709b7fd6172be3bbd149d32a8c6f2
Brian Rosner <brosner@gmail.com>
2009-02-21 19:06:23 -0700

    Use __call__ approach over a root method.

--- dapi/api.py
+++ dapi/api.py
@@ -50,10 +50,6 @@ class Api(object):
             if model not in self._registry:
                 raise NotRegistered('The model %s is not registered' % model.__name__)
             del self._registry[model]
-        
-    def root(self, request, url):
+    
+    def __call__(self, request, url):
         return HttpResponse("hello world")
- 
-# This global object represents the default API, for the common case.
-# You can instantiate Api in your own code to create a custom API.
-site = Api()
--- sample_project/urls.py
+++ sample_project/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import *
 
-import dapi
+from dapi.api import Api
+
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
@@ -16,5 +17,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # (r'^admin/(.*)', admin.site.root),
-    (r'^api/(.*)', dapi.site.root),
+    (r'^api/(.*)', Api()),
 )
