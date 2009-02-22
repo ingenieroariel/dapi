9ad122f95f6c2e54d08084e6293f43379b429ed0
Brian Rosner <brosner@gmail.com>
2009-02-21 19:25:27 -0700

    Enabled the admin and cleaned up sample_project/urls.py a little bit.

--- sample_project/urls.py
+++ sample_project/urls.py
@@ -3,20 +3,11 @@ from django.conf.urls.defaults import *
 import dapi
 dapi.autodiscover()
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^sample_project/', include('sample_project.foo.urls')),
-
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/(.*)', admin.site.root),
-    (r'^api/(.*)', dapi.api.root),
+    url(r'^admin/(.*)', admin.site.root),
+    url(r'^api/(.*)', dapi.api.root),
 )
