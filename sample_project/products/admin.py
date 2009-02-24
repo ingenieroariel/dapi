cf25a5c09bdb490b9d0d4d6e56efa23218720ddf
Ariel Nunez <ingenieroariel@gmail.com>
2009-02-24 11:12:43 -0500

    Added more fields to the products model and created a products.json sample fixture

--- sample_project/products/admin.py
+++ sample_project/products/admin.py
@@ -3,4 +3,8 @@ from django.contrib import admin
 
 from products.models import Product
 
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'description', 'quantity', 'is_cool',
+    'creation_date', 'image', 'discount', 'price')
+
+admin.site.register(Product, ProductAdmin)
--- sample_project/products/models.py
+++ sample_project/products/models.py
@@ -2,4 +2,12 @@
 from django.db import models
 
 class Product(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)    
+    slug = models.SlugField()
+    description = models.TextField()
+    quantity = models.IntegerField()
+    is_cool = models.BooleanField()
+    creation_date = models.DateTimeField()
+    image = models.ImageField(upload_to='products')
+    discount = models.DecimalField(max_digits=3, decimal_places=2)
+    price = models.FloatField()
